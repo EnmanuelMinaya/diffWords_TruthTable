b96cbd0e3e4b91e9da44d1ca4d5d2d03ce5355b6
Enmanuel Minaya <enmanuelminaya@gmail.com>
2022-01-07 16:26:24 -0400

    2nd commit

--- tableoftruth/truthTablePrinter.js
+++ tableoftruth/truthTablePrinter.js
@@ -1,21 +1,31 @@
 
 
+expression = "a|b"
 
+function printTable(expression) {
 
+    let row = []
+    let str = expression;
+    let variables = str.replace(/AND|OR|and|or|XOR|&&|<=|<|>|>=|!=|==|&|OR*|!|[||]{2}|\|/g, '')
+    let values = variables.split('')
+    console.log(values)
+    for (var i = (Math.pow(2, values.length) - 1); i >= 0; i--) {
+        for (var j = (values.length - 1); j >= 0; j--) {
+            row[j] = (i & Math.pow(2, j)) ? '0' : '1'
+        }
 
-function evaluate() {
+        console.log(row)
+    }
 
-    A = 1;
-    B = 0;
-    C = 1;
+    if (values.length === 0) {
+        throw new Error("No variables")
+    }
+    if (values.length === 1) {
 
-    return (eval("A&B|(!C)"));
+    }
 }
 
 
 
 
-
-
-
-module.exports = { evaluate };
+module.exports = { printTable };
--- tableoftruth/truthTablePrinter.test.js
+++ tableoftruth/truthTablePrinter.test.js
@@ -1,9 +1,23 @@
 const { evaluate } = require('./truthTablePrinter.js');
 
-test('should return 0 for A= "A&B|(!C)"', () => {
-    let A = 1;
-    let B = 0;
-    let C = 1;
-    expect(evaluate(A, B, C)).toBe(0);
+test('should return ["0","1"] for variable A', () => {
+    let expression = 'A';
+    expect(printTable(expression)).toEqual(expect.arrayContaining(["0", "1"]));;
 })
+
+test('should return ["0","1"] for variable A', () => {
+    let expression = '!A';
+    expect(printTable(expression)).toEqual(expect.arrayContaining(["1", "0"]));;
+})
+
+test('should return ["0","1"] for variable A', () => {
+    let expression = 'A|B';
+    expect(printTable(expression)).toEqual(expect.arrayContaining([
+        ['0', '0'],
+        ['1', '0'],
+        ['0', '1'],
+        ['1', '1']]));;
+})
+
+
 
